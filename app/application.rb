688a8a6e3f6e0f35ce4d9f7128b90a8ac7b31702
Chaim Reisman <crreisman@gmail.com>
2018-03-09 14:00:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,6 @@
 class Application
-  @@items = [Item.new("Apples", "4000")]
+  @@items = [Item.new("Apples", "4000"),
+  Item.new "Bananas", "546"]
   
   def call(env)
     resp = Rack::Response.new
